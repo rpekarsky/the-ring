dc3e25825902c197af56b864e925dc28c4fb8e21
Roman Pekarsky <contact@rpekarsky.com>
2014-10-19 10:12:50 +0700

    touch update

--- TouchInput.js
+++ TouchInput.js
@@ -7,6 +7,8 @@ var TouchInput = (function(){
 	var moveStart = false
 	var touch = new Victor(gameWidth/2,gameHeight/2);
 	var startVec = touch.clone();
+	var touched = false;
+
 	TouchInput = {
 		init:function(){
 			blob = new PIXI.Sprite.fromFrame('bokeh.png');
@@ -17,7 +19,6 @@ var TouchInput = (function(){
 			blob.tint = 0x000000;
 			blob.alpha = 0.1;
 			blob.visible = false;
-
 
 			touchBlob = new PIXI.Sprite.fromFrame('bokeh.png');
 			touchBlob.pivot.x = touchBlob.width/2;
@@ -45,11 +46,9 @@ var TouchInput = (function(){
 			if(!lastVec){
 				lastVec = vec;
 			}
-
-			var deltaStart = new Victor(touch.x,touch.y).subtract(startVec);
+			var deltaStart = touch.clone().subtract(startVec);
 			var deltaVec = new Victor(lastVec.x,lastVec.y).subtract(vec);
 			var deltaAng = deltaVec.angleDeg();
-
 			var blobVec = new Victor(blob.x,blob.y);
 			var directionVec = blobVec.clone().subtract(touch);
 			var direction = directionVec.clone().norm();
@@ -64,7 +63,10 @@ var TouchInput = (function(){
 			blob.height = radiusMod*2;
 			blob.x += (newPos.x - blob.x)*0.1;
 			blob.y += (newPos.y - blob.y)*0.1;
-			
+			if(moveStart){
+				touchBlob.visible = true;
+				blob.visible = true;
+			}
 			if(moveStart && deltaVec.length() > 30){
 				var CV = (vec.clone().norm().cross(lastVec.norm()) < 0);
 				if(CV){
@@ -73,7 +75,6 @@ var TouchInput = (function(){
 					GoRight();
 				}
 				lastVec = vec;
-
 				var blobSplat = new PIXI.Sprite.fromFrame('bokeh.png');
 				blobSplat.pivot.x = blobSplat.width/2;
 				blobSplat.pivot.y = blobSplat.height/2;
@@ -96,13 +97,17 @@ var TouchInput = (function(){
 				});
 			}
 
-			if(!moveStart && deltaStart.length() > 30){
+			if(!moveStart && deltaStart.length() > 20){
 				blob.alpha = 0.5;
 				moveStart = true;
 				lastVec = vec;
 			}
 		},
 		initEvents:function(){
+			document.addEventListener('mousemove',onmousemove,false);
+			document.addEventListener('mousedown',onmousedown,false);
+			document.addEventListener('mouseup',onmouseup,false);	
+
 			document.addEventListener('touchmove',ontouchmove,false);
 			document.addEventListener('touchstart',ontouchstart,false);
 			document.addEventListener('touchend',ontouchend,false);	
@@ -122,30 +127,39 @@ var TouchInput = (function(){
 		Game.adder.moveUp();
 	}
 
-	function ontouchstart(e){
-		moveStart = false;
-		// touch = new Victor(e.touches[0].pageX,e.touches[0].pageY);
-		// var posx = blob.x-touch.x;
-		// var posy = blob.y-touch.y;
-		// lastVec = new Victor(posx,posy);
-		// startVec = new Victor(touch.x,touch.y);
-		// var blobVec = new Victor(blob.x,blob.y);
-		// var directionVec = blobVec.clone().subtract(touch);
-		// var direction = directionVec.clone().norm();
-		// var directionLength = directionVec.length();
-		// var maxSize = 40;
-		// var centerDistance = new Victor(gameWidth/2,gameHeight/2).distance(blobVec);
-		// var percentDis = (centerDistance/gameHeight)*1.8;
-		// var radiusMod = maxSize - (percentDis)*maxSize;
-		// radiusMod = 20;
-		// var newPos = touch.add(directionVec.norm().multiply({x:radiusMod,y:radiusMod}));
-		// blob.width = radiusMod*2;
-		// blob.height = radiusMod*2;
-		// blob.x = blob.x + (newPos.x - blob.x)*0.5;
-		// blob.y = blob.y + (newPos.y - blob.y)*0.5;
 
-		touchBlob.visible = true;
-		blob.visible = true;
+
+
+	function onmousedown(e){
+		touched = true;
+		moveStart = false;
+		touch = new Victor(e.pageX,e.pageY);
+		startVec = touch.clone();
+	}
+
+	function onmouseup(e){
+		touched = false;
+		if(moveStart == false){
+			GoUp();
+		}
+		touch = new Victor(gameWidth/2,gameHeight/2);
+		startVec = touch.clone();
+		moveStart = false;
+		touchBlob.visible = false
+		blob.visible = false;
+		blob.alpha = 0.1;
+	}
+
+	function onmousemove(e){
+		if(touched){
+			touch = new Victor(e.pageX,e.pageY);
+		}
+	}
+
+	function ontouchstart(e){
+		touch = new Victor(e.touches[0].pageX,e.touches[0].pageY);
+		startVec = touch.clone();
+		moveStart = false;
 	}
 
 	function ontouchend(e){
@@ -153,8 +167,7 @@ var TouchInput = (function(){
 			GoUp();
 		}
 		touch = new Victor(gameWidth/2,gameHeight/2);
-		// blob.x = gameWidth/2;
-		// blob.y = gameHeight/2;
+		startVec = touch.clone();
 		moveStart = false;
 		touchBlob.visible = false
 		blob.visible = false;
@@ -163,74 +176,6 @@ var TouchInput = (function(){
 
 	function ontouchmove(e){
 		touch = new Victor(e.touches[0].pageX,e.touches[0].pageY);
-
-		// var posx = blob.x-touch.x;
-		// var posy = blob.y-touch.y;
-		// var vec = new Victor(posx,posy);
-
-		// touchBlob.x = touch.x;
-		// touchBlob.y = touch.y;
-		// touchBlob.visible = true;
-		// blob.visible = true;
-		// if(!lastVec){
-		// 	lastVec = vec;
-		// }
-
-		// var deltaStart = new Victor(touch.x,touch.y).subtract(startVec);
-		// var deltaVec = new Victor(lastVec.x,lastVec.y).subtract(vec);
-		// var deltaAng = deltaVec.angleDeg();
-
-		// var blobVec = new Victor(blob.x,blob.y);
-		// var directionVec = blobVec.clone().subtract(touch);
-		// var direction = directionVec.clone().norm();
-		// var directionLength = directionVec.length();
-		// var centerDistance = new Victor(gameWidth/2,gameHeight/2).distance(blobVec);
-		// var maxSize = 120;
-		// var percentDis = (centerDistance/gameHeight)*1.8;
-		// var radiusMod = maxSize - (percentDis)*maxSize;
-		// var radiusMod = 30;
-		// var newPos = touch.add(directionVec.norm().multiply({x:radiusMod,y:radiusMod}));
-		// blob.width = radiusMod*2;
-		// blob.height = radiusMod*2;
-		// blob.x += (newPos.x - blob.x)*0.1;
-		// blob.y += (newPos.y - blob.y)*0.1;
-		
-		// if(moveStart && deltaVec.length() > 30){
-		// 	var CV = (vec.clone().norm().cross(lastVec.norm()) < 0);
-		// 	if(CV){
-		// 		GoLeft();
-		// 	} else {
-		// 		GoRight();
-		// 	}
-		// 	lastVec = vec;
-
-		// 	var blobSplat = new PIXI.Sprite.fromFrame('bokeh.png');
-		// 	blobSplat.pivot.x = blobSplat.width/2;
-		// 	blobSplat.pivot.y = blobSplat.height/2;
-		// 	blobSplat.x = touch.x;
-		// 	blobSplat.y = touch.y;
-		// 	blobSplat.tint = 0x000000;
-		// 	blobSplat.alpha = 0.3;
-		// 	blobSplat.width = 60;
-		// 	blobSplat.height = 60;
-
-		// 	basestage.addChild(blobSplat);
-
-		// 	TweenLite.to(blobSplat,0.3,{
-		// 		alpha:0.03,
-		// 		width:200,
-		// 		height:200,
-		// 		onComplete:function(){
-		// 			basestage.removeChild(blobSplat)
-		// 		}
-		// 	});
-		// }
-
-		// if(!moveStart && deltaStart.length() > 30){
-		// 	blob.alpha = 0.5;
-		// 	moveStart = true;
-		// 	lastVec = vec;
-		// }
 	}
 
 	return TouchInput;
